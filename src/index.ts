fe64ded3e666bf0869986c6f4edc898602bf9a8e
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-11-17 08:30:25 +0000

    so far

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import Puppeteer from "puppeteer";
 
+const randomIntFromInterval = (min:number, max:number) => {
+    return Math.floor(Math.random() * (max - min ) + min);
+}
+
+
+let sleep_for = async (page:Puppeteer.Page, min: number, max:number) => {
+    let sleep_duration = randomIntFromInterval(min,max);
+    console.log('waiting for', sleep_duration / 1000, 'seconds');
+    await page.waitForTimeout(sleep_duration); 
+}
 
 let main_actual = async () => {
     try{
@@ -7,8 +17,12 @@ let main_actual = async () => {
         const page = await browser.newPage();
         const URL = 'https://twitter.com/login'
         await page.setViewport({
-            width: 1280, height:800
-        })
+            width: 1280, height:800,
+            deviceScaleFactor:1,
+        });
+
+        await page.goto(URL, {waitUntil: 'networkidle2'});
+        await sleep_for(page, 1000, 2000);
     }catch (e){
         console.log(e);
     }
